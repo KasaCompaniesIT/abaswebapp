9ac20bae893fc9b38618f121db9a6465579fbce1
Adam Staggenborg <astaggenborg@kasacompanies.com>
2025-02-03 16:07:08 -0600

    update import code to match separate import script. update stylesheet update workslip screen

--- admin.py
+++ admin.py
@@ -1,4 +1,4 @@
-import os, csv
+import os, csv, time
 import pandas as pd
 import pyodbc
 
@@ -29,6 +29,11 @@ def importResults():
 @login_required
 def importUserCSV():    
     if request.method == 'POST':        
+        updateCount = 0
+        newCount = 0
+
+        startTime = time.time()
+
         try:
             file = request.files['csvEFile']
             db = get_db()
@@ -45,6 +50,8 @@ def importUserCSV():
                         (row['ID'], row['EMP'], row['NAME'], row['DEPT'], row['SUPERVISOR'], row['WG'])
                     )
                     dbc.commit()
+                    print(f"Inserting new employee record: {row['EMP']}")
+                    newCount += 1
                 else:
                     # update existing employee record
                     dbc.execute(
@@ -52,8 +59,13 @@ def importUserCSV():
                         (row['EMP'], row['NAME'], row['DEPT'], row['SUPERVISOR'], row['WG'], row['ID'])
                     )
                     dbc.commit()
-                
-            return render_template('admin/results.html', results="File imported successfully")
+                    print(f"Updating employee record: {row['EMP']}")
+                    updateCount += 1
+
+            currentTime = time.time()
+            elapsedSeconds = currentTime - startTime
+
+            return render_template('admin/results.html', results="File imported successfully", elapsed=elapsedSeconds, newcount=newCount, updatedcount=updateCount)
         except IOError:
             pass         
         except pyodbc.DatabaseError as err:
@@ -68,6 +80,17 @@ def importUserCSV():
 @login_required
 def importCSV():    
     if request.method == 'POST':        
+        updateProjectCount = 0
+        newProjectCount = 0
+
+        updateWOCount = 0
+        newWOCount = 0
+
+        updateWSCount = 0
+        newWSCount = 0
+
+        startTime = time.time()
+
         try:
             file = request.files['csvFile']
             db = get_db()
@@ -76,54 +99,85 @@ def importCSV():
             
             # flash("Importing Labor Ops Data...")
             
+            currentProject = ""
+            currentWO = ""
+            currentWS = ""
+
             for index, row in df.iterrows():
                 projectID = row['project'].replace("P", "")
-                if not existingProject(projectID):
-                    # insert new project record
-                    dbc.execute(
-                        "INSERT INTO projects (ProjectID, ProjectNumber, ProjectDescription) VALUES (?, ?, ?)",
-                        (projectID, row['project'], row['prjdesc'])
-                    )
-                    dbc.commit()
-                else:
-                    # update existing project record
-                    dbc.execute(
-                        "UPDATE projects SET ProjectDescription=? WHERE projectid=?",
-                        (row['prjdesc'], projectID)
-                    )
-                    dbc.commit()
+                if currentProject != projectID:
+                    if not existingProject(projectID):
+                        # insert new project record
+                        dbc.execute(
+                            "INSERT INTO projects (ProjectID, ProjectNumber, ProjectDescription) VALUES (?, ?, ?)",
+                            (projectID, row['project'], row['prjdesc'])
+                        )
+                        dbc.commit()
+                        print(f"Inserting new project record: {row['project']}")
+                        newProjectCount += 1
+                        currentProject = projectID
+                    else:
+                        # update existing project record
+                        dbc.execute(
+                            "UPDATE projects SET ProjectDescription=? WHERE projectid=?",
+                            (row['prjdesc'], projectID)
+                        )
+                        dbc.commit()
+                        print(f"Updating project record: {row['project']}")
+                        updateProjectCount += 1
+                        currentProject = projectID
 
-                if not existingWO(row['wo']):
-                    # insert new workorder
-                    dbc.execute(
-                        "INSERT INTO workorders (ProjectID, WONumber, WODescription, WOPart) VALUES (?, ?, ?, ?)",
-                        (projectID, row['wo'], row['wodesc'], row['wopart'])
-                    )
-                    dbc.commit()
-                else:
-                    # update existing workorder
-                    dbc.execute(
-                        "UPDATE workorders SET WODescription=?, WOPart=? WHERE wonumber=?",
-                        (row['wodesc'], row['wopart'], row['wo'])
-                    )
-                    dbc.commit()
+                workorderID = row['wo']
+                if currentWO != workorderID:
+                    if not existingWO(row['wo']):
+                        # insert new workorder
+                        dbc.execute(
+                            "INSERT INTO workorders (ProjectID, WONumber, WODescription, WOPart) VALUES (?, ?, ?, ?)",
+                            (projectID, row['wo'], row['wodesc'], row['wopart'])
+                        )
+                        dbc.commit()
+                        print(f"Inserting new workorder record: {row['wo']}")
+                        newWOCount += 1
+                        currentWO = workorderID
+                    else:
+                        # update existing workorder
+                        dbc.execute(
+                            "UPDATE workorders SET WODescription=?, WOPart=? WHERE wonumber=?",
+                            (row['wodesc'], row['wopart'], row['wo'])
+                        )
+                        dbc.commit()
+                        print(f"Updating workorder record: {row['wo']}")
+                        updateWOCount += 1
+                        currentWO = workorderID
 
-                if not existingWS(row['wrkslp']):
-                    # insert new workslip
-                    dbc.execute(
-                        "INSERT INTO workslips (WSNumber, WONumber, WSDescription, WSDockDate, OpID) VALUES (?, ?, ?, ?, ?)",
-                        (row['wrkslp'], row['wo'], row['wsdesc'], row['wsdockdate'], row['wslabop'])
-                    )
-                    dbc.commit()
-                else:
-                    # update new workslip
-                    dbc.execute(
-                        "UPDATE workslips SET WSDescription=?, WSDockDate=?, OpID=? WHERE WSNumber=?",
-                        (row['wsdesc'], row['wsdockdate'], row['wslabop'], row['wrkslp'])
-                    )
-                    dbc.commit()
+                workslipID = row['wrkslp']
+                if workslipID != currentWS:
+                    if not existingWS(row['wrkslp']):
+                        # insert new workslip
+                        dbc.execute(
+                            "INSERT INTO workslips (WSNumber, WONumber, WSDescription, WSDockDate, OpID) VALUES (?, ?, ?, ?, ?)",
+                            (row['wrkslp'], row['wo'], row['wsdesc'], row['wsdockdate'], row['wslabop'])
+                        )
+                        dbc.commit()
+                        print(f"Inserting new workslip record: {row['wrkslp']}")
+                        newWSCount += 1
+                        currentWS = workslipID
+                    else:
+                        # update new workslip
+                        dbc.execute(
+                            "UPDATE workslips SET WSDescription=?, WSDockDate=?, OpID=? WHERE WSNumber=?",
+                            (row['wsdesc'], row['wsdockdate'], row['wslabop'], row['wrkslp'])
+                        )
+                        dbc.commit()
+                        print(f"Updating workslip record: {row['wrkslp']}")
+                        updateWSCount += 1
+                        currentWS = workslipID
 
-            return render_template('admin/results.html', results="File imported successfully")          
+            currentTime = time.time()
+            elapsedSeconds = currentTime - startTime
+
+            return render_template('admin/results.html', results="File imported successfully", elapsed=elapsedSeconds, newprojects=newProjectCount, updatedprojects=updateProjectCount, newwo=newWOCount, updatewo=updateWOCount, newws=newWSCount, updatews=updateWSCount)          
+        
         except IOError:
             pass         
         except pyodbc.DatabaseError as err:
