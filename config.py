76f7dd16ffedc399c65b899052a0a3298f501c2f
Adam Staggenborg <astaggenborg@kasacompanies.com>
2025-05-12 15:47:23 -0500

    update config.py to use new database connection string

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+import os
 
-DB_URL = 'DRIVER={SQL Server};SERVER=kasa-sql.kasa.kasacontrols.com;DATABASE=keserp;UID=abas_webapp;PWD=Autograph-Filtrate8-Fester-Synopsis;Encrypt=no;'
+# Determine the environment (default to 'development')
+ENV = os.getenv('ENV', 'development')
+
+# Use Microsoft ODBC Driver for SQL Server in all environments
+DB_URL = 'DRIVER={ODBC Driver 18 for SQL Server};SERVER=kasa-sql.kasa.kasacontrols.com;DATABASE=keserp;UID=abas_webapp;PWD=Autograph-Filtrate8-Fester-Synopsis;Encrypt=no;'
+
 ABAS_SERVER = r"\\abas\kesdemo\LABOR_IMPORT\\"
