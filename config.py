cba1f10b75815b2aab2c90ef59fffb47ad0471f5
KasaCompaniesIT <it@kasacompanies.com>
2025-05-12 18:27:50 -0500

    Delete config.py

--- config.py
+++
@@ -1,2 +0,0 @@
-# Use Microsoft ODBC Driver for SQL Server in all environments
-DB_URL = 'DRIVER={ODBC Driver 18 for SQL Server};SERVER=kasa-sql.kasa.kasacontrols.com;DATABASE=keserp;UID=abas_webapp;PWD=Autograph-Filtrate8-Fester-Synopsis;Encrypt=no;'
